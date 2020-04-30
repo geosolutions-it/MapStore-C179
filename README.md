7da1b8851958a291468bf6e9bf6532f709ec00ea
mv88 <matteo.velludini.geosolutions@gmail.com>
2020-04-30 17:05:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+PUBLIACQUA
+==========
+
+Quick Start
+------------
+
+Clone the repository with the --recursive option to automatically clone submodules:
+
+`git clone --recursive git@github.com:geosolutions-it/MapStore-C179.git`
+
+Install NodeJS >= 7.10.0 , if needed, from [here](https://nodejs.org/en/download/releases/).
+
+Start the development application locally:
+
+`npm install`
+
+`npm start`
+
+The application runs at `http://localhost:8081` afterwards.
+
+Read more on the [wiki](git@github.com:geosolutions-it/MapStore-C179.git/wiki).
---
+++ prod-webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+
+const themeEntries = require('./MapStore2/build/themes.js').themeEntries;
+const extractThemesPlugin = require('./MapStore2/build/themes.js').extractThemesPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+    base: __dirname,
+    dist: path.join(__dirname, "dist"),
+    framework: path.join(__dirname, "MapStore2", "web", "client"),
+    code: [path.join(__dirname, "js"), path.join(__dirname, "MapStore2", "web", "client")]
+};
+
+module.exports = require('./MapStore2/build/buildConfig')(
+    {
+        'MapStore-C179': path.join(__dirname, "js", "app"),
+        'MapStore-C179-embedded': path.join(__dirname, "MapStore2", "web", "client", "product", "embedded"),
+        'MapStore-C179-api': path.join(__dirname, "MapStore2", "web", "client", "product", "api")
+    },
+    themeEntries,
+    paths,
+    extractThemesPlugin,
+    true,
+    "dist/",
+    '.MapStore-C179',
+    [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'indexTemplate.html'),
+            chunks: ['MapStore-C179'],
+            inject: true,
+            hash: true
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'embeddedTemplate.html'),
+            chunks: ['MapStore-C179-embedded'],
+            inject: true,
+            hash: true,
+            filename: 'embedded.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'apiTemplate.html'),
+            chunks: ['MapStore-C179-api'],
+            inject: 'head',
+            hash: true,
+            filename: 'api.html'
+        })
+    ],
+    {
+        "@mapstore": path.resolve(__dirname, "MapStore2", "web", "client"),
+        "@js": path.resolve(__dirname, "js")
+    }
+);
