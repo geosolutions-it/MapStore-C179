321522352100d88352a52a67bce33f00e56188b4
MV88 <matteo.velludini@gmail.com>
2024-01-18 23:16:23 +0100

    tentative fix #5

--- prod-webpack.config.js
+++ prod-webpack.config.js
@@ -87,8 +87,8 @@ cfg.resolve.fallback = {
     "http": false,
     "https": false,
     "stream": false,
-    "crypto": false/* ,
-    "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify */
+    "crypto": false,
+    "crypto-browserify": require.resolve('crypto-browserify')
 };
 
 module.exports = cfg;
